aa36251a983c9389255d04984d81ea09c9e55875
Noelle <noemaingi@gmail.com>
2024-08-06 16:12:15 +0300

    🗑 update .gitignore and remove ignored files

--- android/app/src/main/kotlin/com/example/number_counter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.number_counter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/noelle/counter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.noelle.counter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
